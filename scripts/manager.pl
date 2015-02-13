0d65201ee173b2521835bfbe26748dc23f3a39de
Kjetil Kjernsmo <kjetil@kjernsmo.net>
2015-02-13 17:43:59 +0100

    just inline the callback

--- scripts/manager.pl
+++ scripts/manager.pl
@@ -1,19 +1,19 @@
 #!/usr/bin/perl
 
-use RDF::QueryX::Cache::Manager;
 use Redis::Fast;
 use CHI;
 use LWP::UserAgent::CHICaching;
 
 my $redis = Redis::Fast->new;
-my $ua = LWP::UserAgent::CHICaching->(cache => CHI->new( driver => 'Memory', global => 1 ));
+my $ua = LWP::UserAgent::CHICaching->new(cache => CHI->new( driver => 'Memory', global => 1 ));
 
-$redis->subscribe('prefetch.queries', &execute);
+$redis->subscribe('prefetch.queries', 
+						# This will cache the query response, with the URL in the message as the key
+						sub {
+							my ($message) = shift;
+							warn $message;
+							return $ua->get($message)->is_success;
+						}
+					  );
 
 $redis->wait_for_messages(2) while 1;
-
-# This will cache the query response, with the URL in the message as the key
-sub execute {
-	my ($message) = shift;
-	return $ua->get($message)->is_success;
-}
